33d5b1e2c56b3eb0ecfaf0b4d2e70778264b2700
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-12-02 14:50:46 +0330

    gateway set

---
+++ Backend/src/gateway/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// افزودن Reverse Proxy
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.Run();
